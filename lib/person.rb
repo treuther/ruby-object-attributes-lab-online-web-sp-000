8412a7c9bb3d10329f819bf8f9fcb2286680738e
Tracy Reuther <tracyreuther@gmail.com>
2019-06-16 23:38:07 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -7,7 +7,7 @@ class Person
   def name=(new_name)
     @name = new_name
   end
-  
+
   def job
     @job
   end
@@ -15,5 +15,5 @@ class Person
   def job=(new_job)
     @job = new_job
   end
-  
+
 end
