43180f804c79c6ab403ce11a919aee86fc86c1af
Tracy Reuther <tracyreuther@gmail.com>
2019-06-16 23:33:36 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,16 +1,16 @@
 class Dog
 
-  def initialize(name)
-    @name = name
-  end
+  # def initialize(name)
+  #   @name = name
+  # end
 
   def name
     @name
   end
 
-  # def name=(new_name)
-  #   @name = new_name
-  # end
+  def name=(new_name)
+    @name = new_name
+  end
 
 end
 
